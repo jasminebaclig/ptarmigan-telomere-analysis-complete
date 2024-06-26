ef3ddd3a8b697c7ab5abf95c8488ccd6a694554c
jasminebaclig <jasminebaclig@yahoo.com>
2024-06-26 12:59:32 -0600

    Saves changes to GitHub

--- statistics_graphs.R
+++ statistics_graphs.R
@@ -9,29 +9,17 @@ library(outliers)
 
 
 
-#Performs Grubbs' Test on potential outliers and removes them from data frame
-grubbs.test(lm_data$rtl) #G = 34.328657, p < 2.2e-16 (12-072 is an outlier)
-no_outlier_data <- filter(lm_data, id != "12-072")
-grubbs.test(no_outlier_data$rtl) #G = 3.4509e+01, p < 2.2e-16 (12-036 is an outlier)
-no_outlier_data <- filter(no_outlier_data, id != "12-036")
-grubbs.test(no_outlier_data$rtl) #G = 28.04866, p < 2.2e-16 (09-002 is an outlier)
-no_outlier_data <- filter(no_outlier_data, id != "09-002")
-grubbs.test(no_outlier_data$rtl) #G = 22.52881, p < 2.2e-16 (09-010 is an outlier)
-no_outlier_data <- filter(no_outlier_data, id != "09-010")
-grubbs.test(no_outlier_data$rtl) #G = 13.49175, p < 2.2e-16 (08-059 is an outlier)
-no_outlier_data <- filter(no_outlier_data, id != "08-059")
-grubbs.test(no_outlier_data$rtl) #G = 14.13032, p < 2.2e-16 (09-072 is an outlier)
-no_outlier_data <- filter(no_outlier_data, id != "09-072")
-grubbs.test(no_outlier_data$rtl) #G = 10.38871, p < 2.2e-16 (09-016 is an outlier)
-no_outlier_data <- filter(no_outlier_data, id != "09-016")
-grubbs.test(no_outlier_data$rtl) #G = 10.38871, p < 2.2e-16 (09-016 is an outlier)
+hist(lm_data$rtl)
+
+no_outlier_data <- filter(lm_data, id != "12-072", id != "12-036")
+
+
 
 male_data <- filter(lm_data, sex == "male")
 female_data <- filter(lm_data, sex == "female")
 
-grubbs.test(male_data$rtl)
-
-
+hist(filter(male_data, rtl < 5)$rtl)
+hist(filter(female_data, rtl < 10)$rtl)
 
 #Compares variances of RTL between sexes using F-test
 var.test(rtl ~ sex, data = lm_data) #F = 8.9967e-07, p < 2.2e-16 (Variances are not equal)
@@ -39,36 +27,82 @@ var.test(rtl ~ sex, data = lm_data) #F = 8.9967e-07, p < 2.2e-16 (Variances are 
 #Performs Welch two-sample t-test
 t.test(rtl ~ sex, data = lm_data, var.equal = FALSE) #t = -1.1101, p = 0.2674 (Means are not different???)
 
-#Creates violin plot of RTL values by sex each year
-rtl_by_sex <- ggplot(lm_data, aes(x = sex, y = rtl)) + 
-              geom_violin(aes(fill = sex)) +
-              geom_beeswarm(size = 2, cex = 2, alpha = 0.7) +
-              guides(fill = FALSE) +
-              scale_fill_manual(values = c("#796C9D", "#F5AD29")) +
-              labs(title = "Relative Telomere Lengths Are Higher Overall\nin Female Ptarmigans", x = NULL, y = "Relative Telomere Length") +
-              theme(plot.title = element_text(face = "bold", hjust = 0.5, size = 25), axis.title = element_text(size = 15), axis.text = element_text(size = 12)) +
-              scale_x_discrete(labels = c("Female", "Male")) +
-              scale_y_continuous(breaks = seq(0, 4, 1), minor_breaks = seq(0, 4, 0.5))
-rtl_by_sex
+#Creates box plot of RTL values by sex each year
+ggplot(no_outlier_data, aes(x = sex, y = rtl)) + geom_boxplot()
 
 
+
+y2006_data <- filter(lm_data, year == "2006")
+y2007_data <- filter(lm_data, year == "2007")
+y2008_data <- filter(lm_data, year == "2008")
+y2009_data <- filter(lm_data, year == "2009")
+y2010_data <- filter(lm_data, year == "2010")
+y2011_data <- filter(lm_data, year == "2011")
+y2012_data <- filter(lm_data, year == "2012")
+y2013_data <- filter(lm_data, year == "2013")
+y2014_data <- filter(lm_data, year == "2014")
+y2015_data <- filter(lm_data, year == "2015")
+y2016_data <- filter(lm_data, year == "2016")
+y2017_data <- filter(lm_data, year == "2017")
+y2018_data <- filter(lm_data, year == "2018")
+
+hist(y2006_data$rtl)
+hist(y2007_data$rtl)
+hist(y2008_data$rtl)
+hist(y2009_data$rtl)
+hist(y2010_data$rtl)
+hist(y2011_data$rtl)
+hist(y2012_data$rtl)
+hist(y2013_data$rtl)
+hist(y2014_data$rtl)
+hist(y2015_data$rtl)
+hist(y2016_data$rtl)
+hist(y2017_data$rtl)
+hist(y2018_data$rtl)
 
 #Compares variances of RTL between years using Bartlett's test
-bartlett.test(rtl ~ year, lm_data) #K-squared = 20.208, p = 0.005137 (Variances are not equal)
+bartlett.test(rtl ~ year, lm_data) #K-squared = 15617, p < 2.2e-16 (Variances are not equal)
 
 #Performs Mood's median test
-median_test(rtl ~ year, data = lm_data) #chi-squared = 26.548, p = 0.0004018 (Medians are different)
+median_test(rtl ~ year, data = lm_data) #chi-squared = 167.44, p < 2.2e-16 (Medians are different)
 
 #Creates violin plot of RTL values by year
-rtl_by_year <- ggplot(lm_data, aes(x = year, y = rtl)) +
-               geom_violin(width = 1.25, aes(fill = year)) +
-               geom_beeswarm(size = 1.5, cex = 1.5, alpha = 0.7) +
-               guides(fill = "none") +
-               scale_fill_manual(values = c("#796C9D", "#8B758C", "#9C7F7C", "#AE886B", "#C0915B", "#D29A4A", "#E3A43A", "#F5AD29")) +
-               labs(title = "Relative Telomere Lengths Are Different\nThroughout 2006 to 2016", x = NULL, y = "Relative Telomere Length") +
-               theme(plot.title = element_text(face = "bold", hjust = 0.5, size = 25), axis.title = element_text(size = 15), axis.text = element_text(size = 12)) +
-               scale_y_continuous(breaks = seq(0, 4, 1), minor_breaks = seq(0, 4, 0.5))
-rtl_by_year
+ggplot(no_outlier_data, aes(x = year, y = rtl)) + geom_boxplot()
+
+
+
+combined_data <- left_join(lm_data, select(combined_kpm, -year), by = join_by(id == id)) %>%
+                 left_join(select(combined_amanda, -year, -WeightNE, -Wing, -Head), by = join_by(id == BirdID))
+
+ggplot(combined_data %>% filter(id != "12-072", id != "12-036"), aes(x = BC.index, y = rtl)) + geom_point()
+summary(lm(rtl ~ BC.index, data = combined_data)) #R-squared = 0.001656, p = 0.2125
+
+ggplot(combined_data %>% filter(id != "12-072", id != "12-036"), aes(x = TotalFat, y = rtl)) + geom_point()
+summary(lm(rtl ~ TotalFat, data = combined_data)) #R-squared = 0.0005338, p = 0.4415
+
+ggplot(combined_data %>% filter(id != "12-072", id != "12-036"), aes(x = WeightNE, y = rtl)) + geom_point()
+summary(lm(rtl ~ WeightNE, data = combined_data)) #R-squared = 0.003785, p = 0.03345
+
+ggplot(combined_data %>% filter(id != "12-072", id != "12-036"), aes(x = Clostridia.UCG.014.Relative.Abundance, y = rtl)) + geom_point()
+summary(lm(rtl ~ Clostridia.UCG.014.Relative.Abundance, data = combined_data)) #R-squared = 0.002426, p = 0.2005
+
+ggplot(combined_data %>% filter(id != "12-072", id != "12-036"), aes(x = Shuttleworthia.Relative.Abundance, y = rtl)) + geom_point()
+summary(lm(rtl ~ Shuttleworthia.Relative.Abundance, data = combined_data)) #R-squared = 0.0001117, p = 0.7837
+
+ggplot(combined_data %>% filter(id != "12-072", id != "12-036"), aes(x = Actinomyces.Relative.Abundance, y = rtl)) + geom_point()
+summary(lm(rtl ~ Actinomyces.Relative.Abundance, data = combined_data)) #R-squared = 0.0001794, p = 0.7279
+
+ggplot(combined_data %>% filter(id != "12-072", id != "12-036"), aes(x = Colidextribacter.Relative.Abundance, y = rtl)) + geom_point()
+summary(lm(rtl ~ Colidextribacter.Relative.Abundance, data = combined_data)) #R-squared = 8.337e-06, p = 0.9402
+
+ggplot(combined_data %>% filter(id != "12-072", id != "12-036"), aes(x = ceca.weight, y = rtl)) + geom_point()
+summary(lm(rtl ~ ceca.weight, data = combined_data)) #R-squared = 0.002473, p = 0.1362
+
+ggplot(combined_data %>% filter(id != "12-072", id != "12-036"), aes(x = ceca.Gut.length, y = rtl)) + geom_point()
+summary(lm(rtl ~ ceca.Gut.length, data = combined_data)) #R-squared = 0.00191, p = 0.1917
+
+ggplot(combined_data %>% filter(id != "12-072", id != "12-036"), aes(x = Shannon.Diversity.Index, y = rtl)) + geom_point()
+summary(lm(rtl ~ Shannon.Diversity.Index, data = combined_data)) #R-squared = 1.895e-05, p = 0.91
 
 
 
@@ -82,16 +116,23 @@ calculate_mean <- function(curr_year, df, col) {
   return(mean(as.numeric(unlist((filter(df, df$year == curr_year) %>% select(col))))))
 }
 
-rtl_mean <- create_mean_table(lm_data, "rtl")
+mean_data <- create_mean_table(lm_data, "rtl") %>% rename(rtl = mean)
+mean_data <- left_join(mean_data, create_mean_table(filter(combined_amanda, !is.na(Standardized.BC.index)), "Standardized.BC.index") %>%
+                                  rename(std_bci = mean), by = join_by(year))
+mean_data <- left_join(mean_data, create_mean_table(filter(combined_kpm, !is.na(TotalFat)), "TotalFat") %>%
+                                  rename(total_fat = mean), by = join_by(year))
+mean_data <- left_join(mean_data, create_mean_table(filter(combined_kpm, !is.na(WeightNE)), "WeightNE") %>%
+                                  rename(weight = mean), by = join_by(year))
+mean_data <- left_join(mean_data, create_mean_table(filter(combined_amanda, !is.na(Clostridia.UCG.014.Relative.Abundance)), "Clostridia.UCG.014.Relative.Abundance") %>%
+                                  rename(clostridia = mean), by = join_by(year))
+mean_data <- left_join(mean_data, create_mean_table(filter(combined_amanda, !is.na(Shuttleworthia.Relative.Abundance)), "Shuttleworthia.Relative.Abundance") %>%
+                                  rename(shuttleworthia = mean), by = join_by(year))
+mean_data <- left_join(mean_data, create_mean_table(filter(combined_amanda, !is.na(Actinomyces.Relative.Abundance)), "Actinomyces.Relative.Abundance") %>%
+                                  rename(actinomyces = mean), by = join_by(year))
+mean_data <- left_join(mean_data, create_mean_table(filter(combined_amanda, !is.na(Colidextribacter.Relative.Abundance)), "Colidextribacter.Relative.Abundance") %>%
+                                  rename(colidexteribacter = mean), by = join_by(year))
 
-bci_mean <- create_mean_table(filter(combined_amanda, !is.na(Standardized.BC.index)), "Standardized.BC.index")
-fat_mean <- create_mean_table(filter(combined_kpm, !is.na(TotalFat)), "TotalFat")
-weight_mean <- create_mean_table(filter(combined_kpm, !is.na(WeightNE)), "WeightNE")
 
-clostridia_mean <- create_mean_table(filter(combined_amanda, !is.na(Clostridia.UCG.014.Relative.Abundance)), "Clostridia.UCG.014.Relative.Abundance")
-shuttleworthia_mean <- create_mean_table(filter(combined_amanda, !is.na(Shuttleworthia.Relative.Abundance)), "Shuttleworthia.Relative.Abundance")
-actinomyces_mean <- create_mean_table(filter(combined_amanda, !is.na(Actinomyces.Relative.Abundance)), "Actinomyces.Relative.Abundance")
-colidextribacter_mean <- create_mean_table(filter(combined_amanda, !is.na(Colidextribacter.Relative.Abundance)), "Colidextribacter.Relative.Abundance")
 
 ceca_weight_mean <- create_mean_table(filter(combined_amanda, !is.na(ceca.weight)), "ceca.weight")
 ceca_gut_mean <- create_mean_table(filter(combined_amanda, !is.na(ceca.Gut.length)), "ceca.Gut.length")
